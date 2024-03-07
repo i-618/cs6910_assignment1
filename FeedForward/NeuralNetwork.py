6cd7d2f506f5a2719d5c995645915153660e19b5
i-618 <you@example.com>
2024-03-07 08:27:12 +0530

    faulty backprop

--- FeedForward/NeuralNetwork.py
+++ FeedForward/NeuralNetwork.py
@@ -1,49 +1,107 @@
 import numpy as np 
-
-
 class NeuralNetwork:
-    def __init__(self, input_dim, output_dim ,nn_archtre: list):
+    def __init__(self, input_dim, output_dim ,nn_archtre: list, last_layer_activation='softmax'):
 
         self.num_layers = len(nn_archtre) + 1
         self.main_model = {'weights': [], 'biases': [], 'activation': []}
         # Initializing First Layer that takes in input_dim
-        self.main_model['weights'].append(np.random.rand(input_dim, nn_archtre[0]['num_neurons']))
+        self.main_model['weights'].append(np.random.rand(nn_archtre[0]['num_neurons'], input_dim))
         self.main_model['biases'].append(np.random.rand())
         self.main_model['activation'].append(nn_archtre[0]['activation'])
 
         # Initializing N - 2 hidden layers 
         for index, layer in enumerate(nn_archtre[1:self.num_layers-1]):
-            self.main_model['weights'].append(np.random.rand(nn_archtre[index]['num_neurons'], layer['num_neurons']))
+            self.main_model['weights'].append(np.random.rand(layer['num_neurons'], nn_archtre[index]['num_neurons']))
             self.main_model['biases'].append(np.random.rand())
             self.main_model['activation'].append(layer['activation'])
 
         # Initializing the last output layer that gives out output_dim
-        self.main_model['weights'].append(np.random.rand(nn_archtre[self.num_layers-2]['num_neurons'], output_dim))
+        self.main_model['weights'].append(np.random.rand(output_dim, nn_archtre[self.num_layers-2]['num_neurons']))
         self.main_model['biases'].append(np.random.rand())
-        self.main_model['activation'].append(nn_archtre[self.num_layers-2]['activation'])
+        self.main_model['activation'].append(last_layer_activation)
 
-        # [print(i.shape) for i in self.main_model['weights']]
+        [print(i.shape, j) for i, j in zip(self.main_model['weights'], self.main_model['activation'])]
+        print('arr_len', len(self.main_model['activation']))
+        print('num_layers', self.num_layers)
+
+    def feed_forward(self, input_data, return_layer_outputs=True):
+            layer_outputs = {'activation':[], 'pre_activation':[]} 
+            # First Layer feed forward
+            final_output = np.dot(self.main_model['weights'][0], input_data) + self.main_model['biases'][0]
+            layer_outputs['pre_activation'].append(final_output)
+            activation = getattr(self, self.main_model['activation'][0], self.sigmoid)
+            final_output = activation(final_output)
+            layer_outputs['activation'].append(final_output)
+            # Rest of the layers feed forward
+            for i in range(1, self.num_layers):
+                final_output = np.dot(self.main_model['weights'][i], final_output) + self.main_model['biases'][i]
+                layer_outputs['pre_activation'].append(final_output)
+                activation = getattr(self, self.main_model['activation'][i], self.sigmoid)
+                final_output = activation(final_output)
+                layer_outputs['activation'].append(final_output)
+            if return_layer_outputs:
+                return layer_outputs
+            else:
+                return final_output
+       
+    
+    def back_propagation(self, x, y):
+        # delta_weights = [np.random.rand(*layer.shape) for layer in self.main_model['weights']]
+        # delta_biases = [np.random.rand() for _ in self.main_model['biases']]
+        delta_weights = [np.zeros_like(layer) for layer in self.main_model['weights']]
+        delta_biases = [np.zeros_like(bias) for bias in self.main_model['biases']]
+        # x, y = train_data['inputs'][0], train_data['labels'][0]
+
+        
+                # feed forward
+        layer_outputs = self.feed_forward(x, return_layer_outputs=True)
+        print('loss', self.loss_mse(y, layer_outputs['activation'][-1]))
+        # print('self num of layers    ', self.num_layers)
+        # print('shape of layer_outputs activation    ', list(map(lambda x: x.shape, layer_outputs['activation'])))
+        # print('shape of layer_outputs pre-activation', list(map(lambda x: x.shape, layer_outputs['pre_activation'])))
+        layer_ouput = layer_outputs['activation'][-1]
+        dAL = - (np.divide(y, layer_ouput) - np.divide(1 - y, 1 - layer_ouput))
+        dZ = dAL * self.sigmoid_derivative(layer_ouput)
+        delta_weights[-1] = 1 / x.shape[0] * np.dot(dZ, layer_ouput.T)
+        delta_biases[-1] = 1 / x.shape[0] * np.sum(dZ, keepdims=True)
+
+        for i in range(2, self.num_layers):
+            
+            dA_prev = np.dot( self.main_model['weights'][-i+1].T, dZ)
+            
+            actv_derivative = getattr(self, self.main_model['activation'][-i] + '_derivative' , self.sigmoid_derivative)
+            
+            dZ = dA_prev * actv_derivative(layer_outputs['pre_activation'][-i])
+            delta_weights[-i] = 1 / x.shape[0] * np.dot(dZ, layer_outputs['activation'][-i].T)
+            delta_biases[-i] = 1 / x.shape[0] * np.sum(dZ, keepdims=True)
+        
         
 
-    def feed_forward(self, input_data):
-        # First Layer feed forward
-        final_output = np.dot(input_data, self.main_model['weights'][0]) + self.main_model['biases'][0]
-        activation = getattr(self, self.main_model['activation'][0], self.sigmoid)
-        final_output = activation(final_output)
-        # Rest of the layers feed forward
-        for i in range(1, self.num_layers):
-            final_output = np.dot(final_output, self.main_model['weights'][i]) + self.main_model['biases'][i]
-            activation = getattr(self, self.main_model['activation'][i], self.sigmoid)
-            final_output = activation(final_output)
-        return final_output
-    
-    def back_propagation(self, train_data):
-        delta_weights = [np.random.rand(*layer.shape) for layer in self.main_model['weights']]
-        delta_biases = [np.random.rand() for _ in self.main_model['biases']]
         return delta_weights, delta_biases
 
     def softmax(self, pre_activation):
         return np.exp(pre_activation) / np.sum(np.exp(pre_activation), axis=0)
+    
+    def softmax_derivative(self, pre_activation):
+        jacobian_m = np.diag(pre_activation)
+
+        for i in range(len(jacobian_m)):
+            for j in range(len(jacobian_m)):
+                if i == j:
+                    jacobian_m[i][j] = pre_activation[i] * (1-pre_activation[i])
+                else: 
+                    jacobian_m[i][j] = -pre_activation[i]*pre_activation[j]
+        return jacobian_m
+
+    
+    def sigmoid_derivative(self, pre_activation):
+        return pre_activation * (1 - pre_activation)
+    
+    def linear_derivative(self, pre_activation):
+        return 1
+    
+    def relu_derivative(self, pre_activation):
+        return 1 * (pre_activation > 0)
     
     def sigmoid(self, pre_activation):
         return 1 / (1 + np.exp(-pre_activation))
@@ -56,10 +114,17 @@ class NeuralNetwork:
 
     def train(self, train_data, epochs, learning_rate):
         print('training started...')
+        if len(train_data['inputs'][0]) != self.main_model['weights'][0].shape[1]:
+            raise ValueError('Input dimension does not match the input layer dimension')
+        elif type(train_data['labels']) != np.ndarray and type(train_data['inputs']) != np.ndarray:
+            train_data['labels'] = np.array(train_data['labels'])
+            train_data['inputs'] = np.array(train_data['inputs'])
         for epoch in range(epochs):
-            delta_weight, delta_bias = self.back_propagation(train_data)
-            self.main_model['weights'] = [self.main_model['weights'][i] - learning_rate * delta_weight[i] for i in range(self.num_layers)]
-            self.main_model['biases'] = [self.main_model['biases'][i] - learning_rate * delta_bias[i] for i in range(self.num_layers)]
+            for x, y in zip(train_data['inputs'], train_data['labels']):
+                delta_weight, delta_bias = self.back_propagation(x, y)
+            # print('delta_weight', delta_weight)
+                self.main_model['weights'] = [self.main_model['weights'][i] - learning_rate * delta_weight[i] for i in range(self.num_layers)]
+                self.main_model['biases'] = [self.main_model['biases'][i] - learning_rate * delta_bias[i] for i in range(self.num_layers)]
         print('training ended...')
         
 
--- main.py
+++ main.py
@@ -6,11 +6,14 @@ layers = [{'num_neurons': 3, 'activation': 'relu'},
           {'num_neurons': 5, 'activation': 'relu'},
           {'num_neurons': 10, 'activation': 'relu'},
           {'num_neurons': 9, 'activation': 'relu'},
-          {'num_neurons': 3, 'activation': 'softmax'},]
-nn = NeuralNetwork(input_dim=2, output_dim=5, nn_archtre=layers)
+          {'num_neurons': 3, 'activation': 'linear'},]
+nn = NeuralNetwork(input_dim=2, output_dim=1, nn_archtre=layers, last_layer_activation='sigmoid')
 
+train_data={'inputs':[[0,0],[0,1],[1,0],[1,1]], 'labels':[[0],[1],[1],[0]]}
 
+print(nn.feed_forward([1, 2], return_layer_outputs=False))
+nn.train(train_data=train_data, epochs=5000, learning_rate=0.8)
+for inp in train_data['inputs']:
+    print(inp, nn.feed_forward(inp, return_layer_outputs=False))
 
-print(nn.feed_forward([1, 2]))
-nn.train(train_data=[[1, 2], [3, 4]], epochs=100, learning_rate=0.01)
-print(nn.feed_forward([1, 2]))
+# print(nn.feed_forward([1, 2]))
